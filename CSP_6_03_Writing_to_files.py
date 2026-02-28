6daef055b7f812c33f49bdcbfc03cd335bc837f1
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-02-28 09:05:34 -0800

    Update CSP_6_03_Writing_to_files.py

--- CSP_6_03_Writing_to_files.py
+++ CSP_6_03_Writing_to_files.py
@@ -1,26 +1,54 @@
 import random
 
-#You need to create your own test file for this assignment.
-#Because we are dealing with both reading and writing to files. Your test file will be more complicated than it has been.
 
 def writeFile(inputList, fileName):
-    #Creates a file of the given name and adds each value from the list to said file with each line being an index from the list.
-    pass
+    # Creates a file of the given name and adds each value from the list
+    # to said file with each line being an index from the list.
+    with open(fileName, "w") as file:
+        for item in inputList:
+            file.write(str(item) + "\n")
 
 
 def sortNames(fileName, targetFile):
-    #Modify the below function such that it takes in source file and a target file.
-    #Sort all of the values from the source file and write them to the target file
-    #I recommend using .sort() for this. You do not need to write the sorting algorithm yourself.
-    pass
+    # Takes in source file and a target file.
+    # Sort all of the values from the source file and write them to the target file.
 
-sortNames("names.txt","namesNew.txt")
+    try:
+        with open(fileName, "r") as file:
+            names = file.readlines()
+
+        names = [name.strip() for name in names]
+
+        names.sort()
+
+        with open(targetFile, "w") as file:
+            for name in names:
+                file.write(name + "\n")
+
+    except FileNotFoundError:
+        print(f"{fileName} not found.")
 
 
+sortNames("names.txt", "namesNew.txt")
 
-def highScore( newScore: int):
-    #Modify the function such that it adds a new score to the file scores.txt
-    #Then return the average score from all of the scores in scores.txt
-    pass
-highScore(random.randint(1,100))
+
+def highScore(newScore: int):
+    # Adds a new score to the file scores.txt
+    # Then returns the average score from all of the scores in scores.txt
+
+    fileName = "scores.txt"
+
+    with open(fileName, "a") as file:
+        file.write(str(newScore) + "\n")
+
+    with open(fileName, "r") as file:
+        scores = file.readlines()
+
+    scores = [int(score.strip()) for score in scores]
+    average = sum(scores) / len(scores)
+
+    return average
+
+
+print("New Average:", highScore(random.randint(1, 100)))
 
